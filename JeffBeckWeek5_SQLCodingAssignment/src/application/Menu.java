5e08b75ad702487d7778dba2b9fb51162d5b0a99
becje <jeff.beck@b5connections.com>
2021-04-19 23:12:45 -0500

    Family Tree - Final Commit
    Foundation for family tree menu driven application backed by a MySQL database
    Java menu drive application allows end user to perform all four CRUD operations on the familyTree sql table....and then some...
    
    This is one of my favorite builds in the class so far. Really like how it runs and the possibilities of what I can do with it for building it out from here...

---
+++ JeffBeckWeek5_SQLCodingAssignment/src/application/Menu.java
@@ -0,0 +1,232 @@
+package application;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import dao.PeopleDao;
+import entity.People;
+
+public class Menu {
+	
+	private PeopleDao peopleDao = new PeopleDao();
+	private Scanner scanner = new Scanner(System.in);
+	private List<String> options = Arrays.asList(
+			"Add New Family Member",
+			"Display Family",
+			"Display Family Members by First Name",
+			"Display Family Members by Last Name",
+			"Update Family Members Name",
+			"Update Relationship Status",
+			"Delete a Family Member");
+	
+	public void start() {
+		String selection = "";
+		
+		do {
+			printMenu();
+			selection = scanner.nextLine();
+			
+			try { 
+				if (selection.equals("1")) {
+					createNewFamilyMember();
+				} else if (selection.equals("2")) {
+					displayFamily();
+				} else if (selection.equals("3")) {
+					lookUpPersonByFirstName();
+				} else if (selection.equals("4")) {
+					lookUpPersonByLasttName();
+				} else if (selection.equals("5")) {
+					updateFamilyMemberName();
+				} else if (selection.equals("6")) {
+					updateRelationship();
+				} else if (selection.equals("7")) {
+					deleteFamilyMember();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}				
+			
+			System.out.println("Press enter to continue......");
+			scanner.nextLine();
+		} while (!selection.equals("-1"));
+	}
+		
+		private void printMenu() {
+			System.out.println("Select an Option:\n------------------------------");
+			for(int i=0; i < options.size(); i++) {
+				System.out.println(i + 1 + ") " + options.get(i));
+		}
+	}
+		private void createNewFamilyMember() throws SQLException {			
+			System.out.println("Enter a numerical unique ID for the new family member: ");
+			int people_id = Integer.parseInt(scanner.nextLine());
+			System.out.println("Enter first name of new family member: ");
+			String firstName = scanner.nextLine();
+			System.out.println("Enter middle name of new family member: ");
+			String middleName = scanner.nextLine();
+			System.out.println("Enter last name of new family member: ");
+			String lastName = scanner.nextLine();
+			System.out.println("Enter maiden name of new family member. Otherwise press enter to continue: ");
+			String maidenName = scanner.nextLine();
+			System.out.println("Enter gender of new family member: ");
+			String gender = scanner.nextLine();
+			System.out.println("Enter the new family members birthday: ");
+			String birthDate = scanner.nextLine();
+			System.out.println("Enter the city the new family member was born in: ");
+			String birthCity = scanner.nextLine();
+			System.out.println("Enter the state the new family member was born in: ");
+			String birthState = scanner.nextLine();
+			System.out.println("Enter the country the new family member was born in: ");
+			String birthCountry = scanner.nextLine();
+			System.out.println("Is this individual is Deceased or Living: ");
+			String lifeStatus = scanner.nextLine();
+			System.out.println("If the individual is deceased enter the date he/she passed away. Otherwise press enter to continue: ");
+			String deathDate = scanner.nextLine();
+			System.out.println("If the individual is deceased enter the cause of death. Otherwise press enter to continue: ");
+			String causeDeath = scanner.nextLine();
+			System.out.println("Enter the age of the new family member as of today. If deceased enter how old he/she was at time of death: ");
+			String age = scanner.nextLine();
+			System.out.println("Enter your relationship with the new family member: ");
+			String relationshipType = scanner.nextLine();
+			peopleDao.createNewFamilyMember(people_id, firstName, middleName, lastName, maidenName, gender, birthDate, birthCity, birthState, birthCountry, lifeStatus, deathDate, causeDeath, age, relationshipType);
+			
+			}
+		private void displayFamily() throws SQLException {
+			List<People> myFamily = peopleDao.getFamily();
+			for (People person : myFamily) {
+				System.out.println(
+					person.getPeopleId() + ": " + " - Name: " +
+					person.getFirstName() + " " +
+					person.getMiddleName() + " " +  
+					person.getLastName() + " " + 
+					person.getMaidenName() + " " + "\n - Gender: " +
+					person.getGender() + " " + "\n - Birthday: " +  
+					person.getBirthDate() + " " + "\n - City of Birth: " +
+					person.getBirthCity() + " " + "\n - State of Birth: " +
+					person.getBirthState() + " " + "\n - Country of Birth: " +
+					person.getBirthCountry() + " " + "\n - Decease or Living: " + 
+					person.getLifeStatus() + " " + "\n - Date of Death: " +
+					person.getDeathDate() + " " + "\n - Cause of Death: " +
+					person.getCauseDeath() + " " + "\n - Age: " +
+					person.getAge() + " " + "\n - Relationship: " +
+					person.getRelationshipType());
+			}
+		}
+		
+		private void lookUpPersonByFirstName() throws SQLException {
+			System.out.println("Type in a first name to show all family members with the same first name: ");
+			String firstNames = scanner.nextLine();
+			List<People> fName = peopleDao.getPersonByFirstName(firstNames);
+			for (People p : fName) {
+				System.out.println(
+					p.getPeopleId() + ": " + " - Name: " +
+					p.getFirstName() + " " +
+					p.getMiddleName() + " " +  
+					p.getLastName() + " " + 
+					p.getMaidenName() + " " + "\n - Gender: " +
+					p.getGender() + " " + "\n - Birthday: " +  
+					p.getBirthDate() + " " + "\n - City of Birth: " +
+					p.getBirthCity() + " " + "\n - State of Birth: " +
+					p.getBirthState() + " " + "\n - Country of Birth: " +
+					p.getBirthCountry() + " " + "\n - Decease or Living: " + 
+					p.getLifeStatus() + " " + "\n - Date of Death: " +
+					p.getDeathDate() + " " + "\n - Cause of Death: " +
+					p.getCauseDeath() + " " + "\n - Age: " +
+					p.getAge() + " " + "\n - Relationship: " +
+					p.getRelationshipType());
+			}
+		}
+		
+		private void lookUpPersonByLasttName() throws SQLException {
+			System.out.println("Type in a last name to show all family members with the same first name: ");
+			String lastNames = scanner.nextLine();
+			List<People> lName = peopleDao.getPersonByLastName(lastNames);
+			for (People p : lName) {
+				System.out.println(
+					p.getPeopleId() + ": " + " - Name: " +
+					p.getFirstName() + " " +
+					p.getMiddleName() + " " +  
+					p.getLastName() + " " + 
+					p.getMaidenName() + " " + "\n - Gender: " +
+					p.getGender() + " " + "\n - Birthday: " +  
+					p.getBirthDate() + " " + "\n - City of Birth: " +
+					p.getBirthCity() + " " + "\n - State of Birth: " +
+					p.getBirthState() + " " + "\n - Country of Birth: " +
+					p.getBirthCountry() + " " + "\n - Decease or Living: " + 
+					p.getLifeStatus() + " " + "\n - Date of Death: " +
+					p.getDeathDate() + " " + "\n - Cause of Death: " +
+					p.getCauseDeath() + " " + "\n - Age: " +
+					p.getAge() + " " + "\n - Relationship: " +
+					p.getRelationshipType());
+			}
+		}
+		
+		private void updateRelationship() throws SQLException {
+			System.out.println("Enter ID of family member you wish to update: ");
+			String n1 = scanner.nextLine();
+			Integer people_id = null;
+			try {
+				people_id = Integer.parseInt(n1);
+			} catch(NumberFormatException e) {
+				System.out.println("Please enter the ID of the family member.");
+				return;
+			}
+			if (people_id !=null) {
+				System.out.println("Please update status to the Relationship: ");
+				String relationshipType = scanner.nextLine();
+				peopleDao.updateRelationship(people_id, relationshipType);
+			}
+		}
+		
+		private void updateFamilyMemberName() throws SQLException {
+			System.out.println("Enter ID of family members name you wish to update: ");
+			String n1 = scanner.nextLine();
+			Integer people_id = null;
+			try {
+				people_id = Integer.parseInt(n1);
+			} catch (NumberFormatException e) {
+				System.out.println("Please enter the ID of the family member.");
+				return;
+			}
+			if (people_id != null) {
+				People currentIndv = peopleDao.getPersonByID(people_id);
+				
+				System.out.println("Please update the First Name or press enter to continue: ");
+				String firstName = scanner.nextLine();
+				if (firstName.isEmpty()) {
+					System.out.println("First Name: " + firstName);
+					firstName = currentIndv.getFirstName();
+				}
+				
+				System.out.println("Please update the Middle Name or press enter to continue: ");
+				String middleName = scanner.nextLine();
+				if (middleName.isEmpty()) {
+					System.out.println("MiddleName: " + middleName);
+					middleName = currentIndv.getMiddleName();
+				}
+			
+				System.out.println("Please update the Last Name or press enter to continue: ");
+				String lastName = scanner.nextLine();
+				if (lastName.isEmpty()) {
+					lastName = currentIndv.getLastName();
+					System.out.println("LastName: " + lastName);
+				}
+		
+				System.out.println("Please update the Maiden Name or press enter to continue: ");
+				String maidenName = scanner.nextLine();
+				if (maidenName.isEmpty()) {
+					System.out.println("MaidenleName: " + maidenName);
+					maidenName = currentIndv.getMaidenName();
+				}
+				peopleDao.updateFamilyMemberName(people_id, firstName, middleName, lastName, maidenName);		
+			}
+		}
+		
+		private void deleteFamilyMember() throws SQLException {
+				System.out.println("Enter the ID of the family member you want to delete: ");
+				int people_id = Integer.parseInt(scanner.nextLine());
+				peopleDao.deletePersonById(people_id);
+			}			
+}
---
+++ JeffBeckWeek5_SQLCodingAssignment/src/dao/PeopleDao.java
@@ -0,0 +1,123 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import entity.People;
+
+public class PeopleDao {
+	
+	private Connection connection;
+	private final String GET_INDIVIDUAL_QUERY = "SELECT * FROM people WHERE people_id=?";
+	private final String GET_FAMILY_QUERY = "SELECT * FROM people";
+	private final String CREATE_NEW_FAMILY_Member_QUERY = "INSERT into people(people_id, firstName, middleName, lastName, maidenName, gender, birthDate, birthCity, birthState, birthCountry, lifeStatus, deathDate, causeDeath, age, relationshipType) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	private final String GET_INDIVIDUAL_BY_FIRST_NAME_QUERY = "SELECT * FROM people WHERE firstName = ?";
+	private final String GET_INDIVIDUAL_BY_LAST_NAME_QUERY = "SELECT * FROM people WHERE lastName = ?";
+	private final String UPDATE_INDIVIDUAL_BY_ID_QUERY = "UPDATE people SET firstName=?, middleName=?, lastName=?, maidenName=? WHERE people_id=?";
+	private final String UPDATE_RELATIONSHIP_QUERY = "UPDATE people SET relationshipType=? WHERE people_id=?";
+	private final String DELETE_INDIVIDUAL_BY_ID_QUERY = "DELETE FROM people WHERE people_id = ?";
+	
+	public PeopleDao() {
+		connection = DBConnection.getConnection();
+	
+	}
+	
+	public People getPersonByID(int people_id) throws SQLException {
+		//People individual = new People();
+		PreparedStatement ps = connection.prepareStatement(GET_INDIVIDUAL_QUERY);
+		ps.setInt(1, people_id);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		People indivdual = new People(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15));
+		
+		return indivdual;
+	}
+	public List<People> getFamily() throws SQLException {
+		List<People> family = new ArrayList<People>();
+		PreparedStatement ps = connection.prepareStatement(GET_FAMILY_QUERY);
+		ResultSet rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			family.add(new People(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15)));
+		}
+		
+		return family;
+	}
+		
+	public List<People> getPersonByFirstName(String firstName) throws SQLException {
+		List<People> firstNames = new ArrayList<People>();
+		PreparedStatement ps = connection.prepareStatement(GET_INDIVIDUAL_BY_FIRST_NAME_QUERY );
+		ps.setString(1, firstName);
+		ResultSet rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			firstNames.add(new People(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15)));
+		}
+		
+		return firstNames;
+	}
+	
+	public List<People> getPersonByLastName(String lastName) throws SQLException {
+		List<People> lastNames = new ArrayList<People>();
+		PreparedStatement ps = connection.prepareStatement(GET_INDIVIDUAL_BY_LAST_NAME_QUERY);
+		ps.setString(1, lastName);
+		ResultSet rs = ps.executeQuery();
+	
+		while (rs.next()) {
+			lastNames.add(new People(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15)));
+		}
+		
+		return lastNames;
+	}
+	public void createNewFamilyMember(int people_id, String firstName, String middleName, String lastName, String maidenName,
+			String gender, String birthDate, String birthCity, String birthState, String birthCountry,
+			String lifeStatus, String deathDate, String causeDeath, String age, String relationshipType) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(CREATE_NEW_FAMILY_Member_QUERY);
+		ps.setInt(1, people_id);
+		ps.setString(2, firstName);
+		ps.setString(3, middleName);
+		ps.setString(4, lastName);
+		ps.setString(5, maidenName);
+		ps.setString(6, gender);
+		ps.setString(7, birthDate);
+		ps.setString(8, birthCity);
+		ps.setString(9, birthState);
+		ps.setString(10, birthCountry);
+		ps.setString(11, lifeStatus);
+		ps.setString(12, deathDate);
+		ps.setString(13, causeDeath);
+		ps.setString(14, age);
+		ps.setString(15, relationshipType);
+		ps.executeUpdate();
+	}
+
+	public void updateFamilyMemberName(int people_id, String firstName, String middleName, String lastName, String maidenName) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(UPDATE_INDIVIDUAL_BY_ID_QUERY);
+		ps.setString(1, firstName);
+		ps.setString(2, middleName);
+		ps.setString(3, lastName);
+		ps.setString(4, maidenName);
+		ps.setInt(5, people_id);
+		ps.executeUpdate();
+	}
+	
+	public void updateRelationship(int people_id, String relationshipType) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(UPDATE_RELATIONSHIP_QUERY);
+		ps.setString(1, relationshipType);
+		ps.setInt(2, people_id);
+		ps.executeUpdate();
+	}
+	
+	public void deletePersonById(int people_id) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(DELETE_INDIVIDUAL_BY_ID_QUERY);
+		ps.setInt(1, people_id);
+		ps.executeUpdate();
+	}
+
+
+}
